663aa503cdf016392b264cd19fe3f04dab3b4bb5
matheuslanduci <matheuslanduci@gmail.com>
2022-12-22 21:23:16 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -19,7 +19,7 @@ describe('Bcrypt Adapter', () => {
     const sut = makeSut()
 
     const hashSpy = jest.spyOn(bcrypt, 'hash')
-    
+
     await sut.encrypt('any_value')
 
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
@@ -27,9 +27,21 @@ describe('Bcrypt Adapter', () => {
 
   test('Should return a hash on success', async () => {
     const sut = makeSut()
-    
+
     const hash = await sut.encrypt('any_value')
 
     expect(hash).toBe('any_hash')
+  })
+
+  test('Should throw if bcrypt throws', async () => {
+    const sut = makeSut()
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+
+    const promise = sut.encrypt('any_value')
+
+    await expect(promise).rejects.toThrow()
   })
 })
