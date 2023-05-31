f83055834a513149a96fe201865ef99667b0fb40
matheuslanduci <matheuslanduci@gmail.com>
2023-05-30 21:28:27 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -10,13 +10,19 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+
+    await accountCollection.deleteMany({})
+  })
+
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
 
   test('Should return an account on success', async () => {
     const sut = makeSut()
-    
+
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email@mail.com',
